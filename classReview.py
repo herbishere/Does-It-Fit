014cc9abdf525d1eccc5401a56e0e4576440d264
Herb <herbishere@users.noreply.github.com>
2019-06-21 16:46:43 -0500

    Added Comments to classReview.py

--- classReview.py
+++ classReview.py
@@ -1,3 +1,21 @@
+'''
+    File name: classReview.py
+    Author: Herbert Diaz
+    Date created: 6/21/2019
+    Last modified: 6/21/2019
+    Python Version: 3.7.3
+
+    Overview:
+        This class object contains all the relevant variables for a single class review.
+        This includes:
+            > courseName = A string of the name of the course.
+            > time = A string detailing the time required by the course.
+            > timeMin = An int detailing the minimum time required by the course.
+            > timeMax = An int detailing the maximum time required by the course.
+            > difficulty = An int detailing the difficulty rating of the course (1 - 5)
+'''
+
+
 class ClassReview:
     def __init__(self, courseName, time, difficulty):
         self.courseName = courseName
@@ -6,6 +24,10 @@ class ClassReview:
         self.timeMax = self.getTimeMax()
         self.difficulty = difficulty
 
+    # getTimeMin(self)
+    # Purpose: Get the minimum time by the course
+    # Entry: none
+    # Exit: the minimum number of hours required by the course
     def getTimeMin(self):
         timeState = self._convertTimeString()
         if timeState == "LO":
@@ -19,6 +41,10 @@ class ClassReview:
         else:
             return -999
 
+    # getTimeMax(self)
+    # Purpose: Get the maxmum time by the course
+    # Entry: none
+    # Exit: the maximum number of hours required by the course
     def getTimeMax(self):
         timeState = self._convertTimeString()
         if timeState == "LO":
@@ -32,6 +58,10 @@ class ClassReview:
         else:
             return -999
 
+    # _convertTimeString(self)
+    # Purpose: Helper function to convert the time string into a smaller string
+    # Entry: none
+    # Exit: the time strings are converted into a smaller string
     def _convertTimeString(self):
         LO = "0-5 hours"
         MED = "6-12 hours"
