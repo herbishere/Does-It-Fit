55d69ca199d455b6bac5e7f55e8914e997c95e33
Herb <herbishere@users.noreply.github.com>
2019-06-21 16:58:52 -0500

    Added Comments to importReviews.py

--- importReviews.py
+++ importReviews.py
@@ -1,3 +1,18 @@
+'''
+    File name: importReviews.py
+    Author: Herbert Diaz
+    Date created: 6/21/2019
+    Last modified: 6/21/2019
+    Python Version: 3.7.3
+
+    Overview:
+        This class takes the location of a csv file containing the course reviews and 
+        puts the relevant values into a list of ClassReview objects.
+        This includes:
+            > file = A string of the location of the csv file
+            > list = A unsorted list of ClassReview objects
+'''
+
 # IMPORT PACKAGES AND MODULES
 import csv
 from classReview import ClassReview
@@ -8,6 +23,12 @@ class ImportReviews:
         self.file = file
         self.list = self.csvToArray()
 
+    # _getIndices(self, headerList, matchString)
+    # Purpose: Get the Indices of certain strings in a header and returning an array
+    #   of the indices
+    # Entry:    headerList = a list of column names
+    #           matchString = the string of the column name to search for
+    # Exit: An array of indices detailing the location of the matchString
     def _getIndices(self, headerList, matchString):
         indices = []
         index = 0
@@ -17,18 +38,38 @@ class ImportReviews:
             index += 1
         return indices
 
+    # _getCourseNameIndices(self, headerList)
+    # Purpose: Get the Indices of the course name columns and return an array
+    #   of the course name indices
+    # Entry:    headerList = a list of column names
+    # Exit: An array of indices detailing the location of the course names
     def _getCourseNameIndices(self, headerList):
         COURSENAME_HEADER = "What Course Did You Take?"
         return self._getIndices(headerList, COURSENAME_HEADER)
 
+    # _getHoursIndices(self, headerList)
+    # Purpose: Get the Indices of the time columns and return an array
+    #   of the time indices
+    # Entry:    headerList = a list of column names
+    # Exit: An array of indices detailing the location of the times
     def _getHoursIndices(self, headerList):
         HOURS_HEADER = "How much time did you spend on average (per week) for this class?"
         return self._getIndices(headerList, HOURS_HEADER)
 
+    # _getDifficultyIndices(self, headerList)
+    # Purpose: Get the Indices of the difficulty columns and return an array
+    #   of the difficulty indices
+    # Entry:    headerList = a list of column names
+    # Exit: An array of indices detailing the location of the difficultys
     def _getDifficultyIndices(self, headerList):
         DIFFICULTY_HEADER = "How hard was this class?"
         return self._getIndices(headerList, DIFFICULTY_HEADER)
 
+    # csvToArray(self)
+    # Purpose: Take the location of the csv file and put all relevant information into a
+    #   list, which is then returned
+    # Entry: none
+    # Exit: An array of the course reviews.
     def csvToArray(self):
         with open(self.file) as csvFile:
             readCSV = csv.reader(csvFile)
