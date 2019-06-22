445e3ef0f1e4cf49e01fc5049e8dd8818bb069ba
James Scanlon <jamesscanlon96@gmail.com>
2019-06-22 16:17:04 -0500

    Set 419/467 as 467

---
+++ classCourse.py
@@ -0,0 +1,25 @@
+# I treated 419/467 as 467 since that's the online course number
+class Course:
+    def __init__(self):
+        self.CS161 = "CS 161 - Intro to Computer Science I"
+        self.CS162 = "CS 162 - Intro to Computer Science II"
+        self.CS165 = "CS 165 - Intro to Computer Science (Accelerated)"
+        self.CS225 = "CS 225 - Discrete Structures in Computer Science"
+        self.CS261 = "CS 261 - Data Structures"
+        self.CS271 = "CS 271 - Computer Architecture & Assembly Language"
+        self.CS290 = "CS 290 - Web Development"
+        self.CS325 = "CS 325 - Analysis of Algorithms"
+        self.CS340 = "CS 340 - Introduction to Databases"
+        self.CS344 = "CS 344 - Operating Systems"
+        self.CS352 = "CS 352 - Introduction to Usability Engineering"
+        self.CS361 = "CS 361 - Software Engineering I"
+        self.CS362 = "CS 362 - Software Engineering II"
+        self.CS372 = "CS 372 - Intro to Computer Networks"
+        self.CS373 = "CS 373 - Defense Against the Dark Arts"
+        self.CS419 = "CS 419/467 - Software Projects"
+        self.CS464 = "CS 464 - Open Source Software"
+        self.CS467 = "CS 419/467 - Software Projects"
+        self.CS475 = "CS 475 - Intro to Parallel Programming"
+        self.CS496 = "CS 496 - Mobile and Cloud Software Development"
+
+
