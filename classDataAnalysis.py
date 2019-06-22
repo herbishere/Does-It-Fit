d50934fcaab6751b94991e2e30b53ae2e43dd517
Herb <herbishere@users.noreply.github.com>
2019-06-21 21:23:16 -0500

    Update classDataAnalysis.py
    Commented Out

--- classDataAnalysis.py
+++ classDataAnalysis.py
@@ -1,24 +1,25 @@
-from classReview import ClassReview
-from importReviews import ImportReviews
+# MAYBE REDUNDANT DUE TO classStats.py
+#  from classReview import ClassReview
+# from importReviews import ImportReviews
 
 
-class ClassAnalysis:
-    def __init__(self, file):
-        self.reviewList = ImportReviews(file).list
-        self.courseList = self._makeCourseList()
+# class ClassAnalysis:
+#     def __init__(self, file):
+#         self.reviewList = ImportReviews(file).list
+#         self.courseList = self._makeCourseList()
 
-    def _makeCourseList(self):
-        courseList = []
-        for reviews in self.reviewList:
-            if reviews.courseName not in courseList:
-                courseList.append(reviews.courseName)
-        courseList.sort()
-        return courseList
+#     def _makeCourseList(self):
+#         courseList = []
+#         for reviews in self.reviewList:
+#             if reviews.courseName not in courseList:
+#                 courseList.append(reviews.courseName)
+#         courseList.sort()
+#         return courseList
 
 
-# SET DATA DIRECTORY
-DIR = './data'
-FILE = '/Course Reviews (Responses) - Form Responses 1.csv'
-file = '{}{}'.format(DIR, FILE)
+# # SET DATA DIRECTORY
+# DIR = './data'
+# FILE = '/Course Reviews (Responses) - Form Responses 1.csv'
+# file = '{}{}'.format(DIR, FILE)
 
-test = ClassAnalysis(file)
+# test = ClassAnalysis(file)
